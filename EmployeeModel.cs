a3eb13d827a2f5cd14c264478f698f71af38c33d
dabhade904 <dabhade904@gmail.com>
2022-09-07 23:22:33 +0530

    Added : payroll servies project

---
+++ EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    public class EmployeeModel
+    {
+        public string EmployeeID;
+        public string EmployeeName;
+        public string PhoneNumber;
+        public string Address;
+        public string Department;
+        public string Gender;
+        public float BasicPay;
+        public float Deductions;
+        public float TaxablePay;
+        public float Tax;
+        public float NetPay; 
+
+    }
+}
---
+++ EmployeeRepo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    public class EmployeeRepo
+    {
+        public string connection = @"(localdb)\MSSQLLocalDB;Initial Catalog=Employee_Payroll;Integrated Security=True";
+        SqlConnection cmd = new SqlConnection();
+
+        public void CreateDb()
+        {
+            EmployeeModel employeeModel = new EmployeeModel();
+            using (this.connection)
+            {
+                string query=@"select EmployeeID,"
+            }
+        }
+    }
+}
